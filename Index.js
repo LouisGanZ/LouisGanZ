ca4366934f36db394f6069d7848096750dd2cf9d
LouisXVII <99965081+MD-XLouis@users.noreply.github.com>
2022-02-20 12:51:34 +0700

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,22 @@
+{
+  "name": "MD-XLouis",
+  "version": "1.0.7.8",
+  "description": " This repository is made for everyone, and this is my intro to Indonesian",
+  "main": "index.js",
+  "scripts": {
+    "test": "node js.js"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/INDEX-JS/MD-XLouis"
+  },
+  "keywords": [
+    "MD",
+    "-XLouis"
+    " This repository is made for everyone,"
+    "and this is my intro to Indonesian"
+  ],
+  "author": "MD-XLouus <i@MD-XLouis>",
+  "license": "-",
+  "homepage": "https://github.com/MD-XLouis"
+}
